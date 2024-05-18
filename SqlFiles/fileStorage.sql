befc253d38f6a1b5ab3af6e6c81db7da087162e6
ArtLiathain <artp.oliathain@gmail.com>
2024-05-18 23:21:12 +0100

    Add basic server file handling functionality

--- SqlFiles/fileStorage.sql
+++ SqlFiles/fileStorage.sql
@@ -1,7 +1,6 @@
 CREATE TABLE fileStorage (
     filename VARCHAR(255),
     publickey VARCHAR(255),
-    sharedSecret int,
     PRIMARY KEY (filename),
     FOREIGN KEY (publickey) REFERENCES users(publickey)
 )
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const { createServer } = require("node:http");
-const { URL } = require("url");
-const querystring = require("querystring");
-const crypto = require("crypto");
 var mysql = require("mysql");
 const express = require("express");
-
-
+const querystring = require("querystring");
+const crypto = require("crypto");
+const multer = require("multer");
+const path = require("path");
 
 const hostname = "localhost";
 const port = 9022;
@@ -22,21 +20,83 @@ con.connect(function (err) {
   console.log("Connected!");
 });
 
-const app = express ();
+const app = express();
 app.use(express.json());
 
-app.post('/addUser', function(req, res) {
-  let pepper = "NoDictionaryTablesForYou"
-  var sql =
-  `INSERT INTO users VALUES ("${req.body.publickey}", "${req.body.user}", "${req.body.pass}","${req.body.salt}" ,"${pepper}")`;
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });  
-  res.send(req.body);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + ":" + file.originalname);
+  },
 });
 
+app.post("/addUser", function (req, res) {
+  let pepper = "NoDictionaryTablesForYou";
+  var sql = `INSERT INTO users VALUES ("${req.body.publickey}", "${req.body.user}", "${req.body.pass}","${req.body.salt}" ,"${pepper}")`;
+  con.query(sql, function (err, result) {
+    if (err) {
+      res.send({ message: "Value already in database" });
+      return;
+    }
+    console.log("1 record inserted");
+  });
+});
 
+const upload = multer({ storage: storage });
+
+app.post("/addFile", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  console.log(`INSERT INTO fileStorage VALUES ("${req.file.filename}","${req.body.publickey}");`);
+  var sql = `INSERT INTO fileStorage VALUES ("${req.file.filename}","${req.body.publickey}");`;
+  con.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json({ message: "Value already in database" });
+
+      return;
+    }
+    console.log("1 record inserted");
+  });
+
+  res.json({
+    message: "File uploaded successfully",
+    filename: req.file.filename,
+  });
+});
+
+app.get("/allfiles/:publickey", (req, res) => {
+  con.query(
+    `SELECT filename from fileStorage WHERE publickey = ${req.params.publickey}`,
+    (err, rows) => {
+      if (err) {
+        console.log("error retrieving filenames");
+        return res.status(400).json({ error: "No files found" });
+      } else {
+        let listOfFiles = [];
+        for (i = 0; i < rows.length; i++) {
+          listOfFiles.push(rows[i].filename);
+        }
+        res.json({files : listOfFiles});
+      }
+    }
+  );
+});
+
+app.get("/downloadFile/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, "uploads", filename);
+
+  res.download(filePath, (err) => {
+    if (err) {
+      console.error("Error downloading the file: ", err);
+      res.status(404).send("File not found");
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log("Server Listening on PORT:", port);
