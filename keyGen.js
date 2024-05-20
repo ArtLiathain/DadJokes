328bdd5ce7ea77249666c1397094256ff95ec7a7
Darragh-Grealish <dgrealish2003@gmail.com>
2024-05-20 11:17:29 +0100

    Create function for Public & Private Key Gen

--- keyGen.js
+++ keyGen.js
@@ -1,18 +1,20 @@
 // https://mothereff.in/binary-ascii
 // https://easytoolz.us/word-counter?gad_source=1&gclid=CjwKCAjwl4yyBhAgEiwADSEjeOtqIMDyXKcFfuPp7T7HBrP_njIk38z-sXxDGEKHWtZJIKU4AMLTRBoCv4YQAvD_BwE
 
+function createPublicKey() {
+    var crypto = require('crypto');
+    var prime_length = 1536; // 2048 bits
+    var diffHell = crypto.createDiffieHellman(prime_length);
+    diffHell.generateKeys('base64');
+    var publicKey = diffHell.getPublicKey('base64');
+    return publicKey;
+}
 
-var crypto = require('crypto');
-
-var prime_length = 1536; // 2048 bits
-
-var diffHell = crypto.createDiffieHellman(prime_length);
-
-diffHell.generateKeys('base64');
-
-var publicKey = diffHell.getPublicKey('base64');
-var privateKey = diffHell.getPrivateKey('base64');
-
-console.log("Public Key : " ,publicKey);
-console.log("Private Key : " ,privateKey);
-
+function createPrivateKey() {
+    var crypto = require('crypto');
+    var prime_length = 1536; // 2048 bits
+    var diffHell = crypto.createDiffieHellman(prime_length);
+    diffHell.generateKeys('base64');
+    var privateKey = diffHell.getPrivateKey('base64');
+    return privateKey;
+}
