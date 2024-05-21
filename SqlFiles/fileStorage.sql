3e6970402ec282854a00343065e8bbff03a436ae
Art Ó Liatháin <75276011+ArtLiathain@users.noreply.github.com>
2024-05-21 10:34:59 +0100

    Update fileStorage.sql

--- SqlFiles/fileStorage.sql
+++ SqlFiles/fileStorage.sql
@@ -1,6 +1,7 @@
 CREATE TABLE fileStorage (
     filename VARCHAR(255),
-    publickey VARCHAR(255),
+    topublickey VARCHAR(255),
+    frompublickey VARCHAR(255),
     PRIMARY KEY (filename),
     FOREIGN KEY (publickey) REFERENCES users(publickey)
 )
