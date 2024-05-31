e9f02d2996565d39d6c65a28b9d63286a0632785
Darragh-Grealish <dgrealish2003@gmail.com>
2024-05-31 10:39:33 +0100

    Change export function

--- src/keyGen.js
+++ src/keyGen.js
@@ -1,7 +1,4 @@
-// https://mothereff.in/binary-ascii
-// https://easytoolz.us/word-counter?gad_source=1&gclid=CjwKCAjwl4yyBhAgEiwADSEjeOtqIMDyXKcFfuPp7T7HBrP_njIk38z-sXxDGEKHWtZJIKU4AMLTRBoCv4YQAvD_BwE
-
-exports.createPublicKey = function() {
+export function createPublicKey() {
     var crypto = require('crypto');
     var prime_length = 1536; // 2048 bits
     var diffHell = crypto.createDiffieHellman(prime_length);
@@ -10,7 +7,7 @@ exports.createPublicKey = function() {
     return publicKey;
 }
 
-exports.createPrivateKey = function() {
+export function createPrivateKey() {
     var crypto = require('crypto');
     var prime_length = 1536; // 2048 bits
     var diffHell = crypto.createDiffieHellman(prime_length);
