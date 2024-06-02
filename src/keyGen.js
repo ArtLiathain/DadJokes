c0fb1243f8f897abe2799632cfc00b64c9078cfc
Darragh-Grealish <dgrealish2003@gmail.com>
2024-06-02 14:04:34 +0100

    Change to Eliptic Curve Diffie-Hellman

--- src/keyGen.js
+++ src/keyGen.js
@@ -1,17 +1,12 @@
 import crypto from 'crypto';
 
+const ecdh = crypto.createECDH('secp256k1');
+ecdh.generateKeys();
+
 export function createPublicKey() {
-    var prime_length = 1536; // 2048 bits
-    var diffHell = crypto.createDiffieHellman(prime_length);
-    diffHell.generateKeys('base64');
-    var publicKey = diffHell.getPublicKey('base64');
-    return publicKey;
+    return ecdh.getPublicKey('hex');
 }
 
 export function createPrivateKey() {
-    var prime_length = 1536; // 2048 bits
-    var diffHell = crypto.createDiffieHellman(prime_length);
-    diffHell.generateKeys('base64');
-    var privateKey = diffHell.getPrivateKey('base64');
-    return privateKey;
+    return ecdh.getPrivateKey('hex');
 }
