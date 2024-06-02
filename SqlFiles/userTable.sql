9b328aa096c3b838ad0453a250fe8eaaa14afc5a
ArtLiathain <artp.oliathain@gmail.com>
2024-06-02 10:20:14 +0100

    commit to switch to desktop

--- SqlFiles/userTable.sql
+++ SqlFiles/userTable.sql
@@ -2,7 +2,5 @@ CREATE TABLE users (
     publickey VARCHAR(255),
     username VARCHAR(255),
     password VARCHAR(255),
-    salt VARCHAR(255),
-    pepper VARCHAR(255),
     PRIMARY KEY (publickey)
 ) ENGINE=InnoDB;
