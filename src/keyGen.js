27991ddfdcc746bb352d393e54b522c38471780a
Darragh-Grealish <dgrealish2003@gmail.com>
2024-06-04 08:59:53 +0100

    generate public private keys in index.html

--- src/keyGen.js
+++ src/keyGen.js
@@ -1,15 +1,28 @@
-import crypto from "crypto";
-import server from "./server";
+// import crypto from "crypto";
+// import server from "./server";
 
 export async function keyStoreLevelDB(username){
-  const ecdh = crypto.createECDH("secp256k1");
-  const keys = ecdh.generateKeys();
-  const publicKey = keys.getPublicKey("base64");
+//   const ecdh = crypto.createECDH("secp256k1");
+//   const keys = ecdh.generateKeys();
+//   const publicKey = keys.getPublicKey("base64");
+const crypto = window.crypto.subtle;
+  const keys = await crypto.generateKey(
+    {
+      name: "ECDH",
+      namedCurve: "P-256", // equivalent to secp256k1
+    },
+    true,
+    ["deriveKey", "deriveBits"]
+  );
+
+const publicKey = await crypto.exportKey("raw", keys.publicKey);
+const privateKey = await crypto.exportKey("raw", keys.privateKey);
+
   const db = server.db;
   try {
     
     await db.put(`${username} Public`, publicKey);
-    await db.put(`${publicKey} Private`, keys.getPrivateKey("base64"));
+    await db.put(`${publicKey} Private`, privateKey); //keys.getPrivateKey("base64")
     console.log("Successfully put Keys");
   } catch (error) {
     console.log(error);
